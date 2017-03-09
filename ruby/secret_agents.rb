2e1611756a226f170039e95f26f4de1221aa7b17
Iftekhar Alam <redflame1510@msn.com>
2017-03-09 17:54:37 -0500

    Added a decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -16,5 +16,26 @@ def encrypt (string )
 	result 
 end
 
+def decrypt (string )
+	alphabet = ("a".."z").to_a
+	result = "" 
+	idx = 0 
+	while idx < string.length
+		letter = string[idx] 
+		if letter == " "
+			result += " "
+		else
+			j_idx = alphabet.index(letter)
+			k_idx = (j_idx - 1) % alphabet.length 
+			result += alphabet[k_idx]
+		end
+		idx += 1
+	end
+	result 
+end
+
 puts encrypt ("abc")
+puts encrypt ("zed")
+puts decrypt ("bcd")
+puts decrypt ("afe")
 
