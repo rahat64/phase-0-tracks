7c18cec631426de5474aa23b9b4b97ad53be455a
Iftekhar Alam <redflame1510@msn.com>
2017-03-09 18:40:42 -0500

    Updated agents to ask for user input

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -34,8 +34,34 @@ def decrypt (string )
 	result 
 end
 
-puts encrypt ("abc")
-puts encrypt ("zed")
-puts decrypt ("bcd")
-puts decrypt ("afe")
+#puts encrypt ("abc")
+#puts encrypt ("zed")
+#puts decrypt ("bcd")
+#puts decrypt ("afe")
+
+#puts decrypt(encrypt("swordfish"))
+# This method works because we technically call both
+# methods simultaneously where we first call the encrypt
+# method which succeds in encripting the string but 
+# immediately afterwards, the decrypt method is called
+# so the process of encrypt is immediately nullified
+# by the calling of decrypt method
+
+puts "Would you like to decrypt, or encrypt as password?"
+resp = gets.chomp 
+
+until resp == "decrypt" || resp == "encrypt" 
+	puts "encrypt or decrypt?"
+	resp = gets.chomp 
+end 
+
+if resp == "encrypt"
+	puts "Enter password you want encypted"
+	pass = gets.chomp 
+	puts encrypt(pass)
+else
+	puts "Enter password you want decrypted"
+	pass = gets.chomp 
+	puts decrypt(pass)
+end
 
