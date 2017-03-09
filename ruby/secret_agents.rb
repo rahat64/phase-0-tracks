2b9e9371a075263224ba1c80370ed25908ece5ce
Iftekhar Alam <redflame1510@msn.com>
2017-03-09 16:02:13 -0500

    Initial Commit

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,20 @@
+def encrypt (string )
+	alphabet = ("a".."z").to_a
+	result = "" 
+	idx = 0 
+	while idx < string.length
+		letter = string[idx] 
+		if letter == " "
+			result += " "
+		else
+			j_idx = alphabet.index(letter)
+			k_idx = (j_idx + 1) % alphabet.length 
+			result += alphabet[k_idx]
+		end
+		idx += 1
+	end
+	result 
+end
+
+puts encrypt ("abc")
+
