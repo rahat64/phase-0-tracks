1f5469c4a0ff51dd131b2d80e187634930242385
Iftekhar Alam <redflame1510@msn.com>
2017-03-16 15:37:39 -0400

    Add .each and .map methods to both hash and array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,38 @@
 arr = ["cup", "glass", "can"]
 has_h = {
-	soda: ["coke", "pepsi", "sprite"],
-	beer: ["budweiser", "miller", "coors"],
-	juice: ["kool-aid", "caprison", "tropicana"]
+	soda: "coke",
+	beer: "budweiser",
+	juice: "kool-aid"
 }
+
+puts "Original data: "
+p arr
+p has_h
+
+puts "After .each:"
+arr.each { |x| puts "I drink out of #{x}."}
+
+has_h.each { |key, value| puts "My favorite #{key} is #{value}."}
+
+puts "After .map and .map!:"
+arr.map do |x|
+	puts x
+	x.next
+end
+
+p arr
+
+arr.map! do |x|
+	puts x
+	x.reverse
+end
+
+p arr
+
+has_h.map do |key, value|
+  puts "This #{key} is #{value}."
+  value.reverse
+end
+
+p has_h
+#p has_h
