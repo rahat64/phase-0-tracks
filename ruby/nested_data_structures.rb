72cdcb5d7f7fef64f9d8d4a2556434b0472bc9b9
Iftekhar Alam <redflame1510@msn.com>
2017-03-16 21:55:34 -0400

    Created a nested database of a classrom

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,44 @@
+classroom = {
+	room: {
+		total_seats: 19,
+		total_students: 
+	}
+	row_1: {
+		category: "Delinquints",
+		desks: {
+			total_seats: 6,
+			open_seats: 3
+		},
+		students: [
+			"Alex",
+			"Steve",
+			"Rebecca" 
+		]
+	},
+
+	row_2: {
+		category: "Average",
+		desks: {
+			total_seats: 6,
+			open_seats: 2
+		},
+		students: [
+			"Bob",
+			"Tom",
+			"Racheal",
+			"Mike" 
+		]
+	},
+
+	row_3: {
+		category: "Smarts",
+		desks: {
+			total_seats: 6,
+			open_seats: 4
+		},
+		students: [
+			"Bellamy",
+			"Stroenheim"
+		]
+	}
+}
