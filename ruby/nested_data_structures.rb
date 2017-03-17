a7ebb2ca4ef41d059d9acfb3f3e535a09912036c
Iftekhar Alam <redflame1510@msn.com>
2017-03-17 03:12:49 -0400

    Finished adding content to the nested database and printed various information with the given data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,8 +1,11 @@
 classroom = {
 	room: {
 		total_seats: 19,
-		total_students: 
-	}
+		total_students: 9,
+		total_seats_open: 9,
+		teacher_presence: true
+	},
+	
 	row_1: {
 		category: "Delinquints",
 		desks: {
@@ -42,3 +45,46 @@ classroom = {
 		]
 	}
 }
+
+puts "How many many total seats are in the room?"
+p classroom[:room][:total_seats]
+
+puts "Is the teacher present?"
+if classroom[:room][:teacher_presence]
+	puts "yes"
+else
+  	puts "no"
+end
+
+puts "How many students are seated in row 3?"
+p classroom[:row_3][:desks][:total_seats] - classroom[:row_3][:desks][:open_seats]
+
+puts "Need more smart kids, add Jessica to the classroom"
+classroom[:row_3][:students].push("Jessica")
+
+puts "Update any more necessary information with the addition of Jessica and print out a summary:"
+
+new_desks = {
+  total_seats: 6,
+  open_seats: 3
+}
+
+classroom[:row_3][:desks].merge!(new_desks)
+
+new_room = {
+  total_seats: 19,
+	total_students: 10,
+	total_seats_open: 9,
+	teacher_presence: false
+}
+
+classroom[:room].merge!(new_room)
+
+p classroom
+
+puts "Is the teacher still there?"
+if classroom[:room][:teacher_presence]
+	puts "yes"
+else
+  	puts "no"
+end
