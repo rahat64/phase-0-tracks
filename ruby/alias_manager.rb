739c9844e419bd25c3f52f64c8b8da9b49bd4486
Iftekhar Alam <redflame1510@msn.com>
2017-03-19 23:32:02 -0400

    Gave the option to users for entering names and continue enting other names as well

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,28 +2,45 @@ def alias_converter (string1, string2)
   secret = string2 + " " + string1
 end
 
-def next_vowel (string)
-  string = ""
+def next_vowel(str)
   vowels = ["a", "e", "i", "o", "u"]
-  index = 0 
-  while index < string.length
-    if string.include?(vowels)
-      vowels.rotate!
+  str = str.split('')
+  str_new = str.map do |char|
+    if vowels.include?(char)
+      vowels.rotate(1)[vowels.index(char)]
     else
-      string[index].next!
+      char.next
     end
-    index += 1
   end
+  str_new.join
 end
 
-puts "Enter first name."
-first = gets.chomp 
+alias_storage = {
+}
 
-puts "Enter last name."
-second = gets.chomp 
+begin
+  puts "Enter first name."
+  first = gets.chomp
 
-sec_identity = alias_converter(first, second)
+  puts "Enter last name."
+  second = gets.chomp 
 
-next_vowel(sec_identity)
+  real_name = first + " " + second
+  puts "Real name is #{real_name}."
 
-p sec_identity
+  alias1 = alias_converter(first, second)
+  
+  alias2 = next_vowel(alias1)
+  
+  p alias2
+  
+  alias_storage[alias2] = real_name
+  
+  puts "Press any key to continue adding names, otherwise type done"
+  resp = gets.chomp
+  
+end until resp == "done"
+
+alias_storage.each do |alias2, real_name|
+  puts "#{alias2} is actually #{real_name} in disguise."
+end
