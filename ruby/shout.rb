11e9eed89d83fb440dd2b9313f83a3ff97494f8a
Iftekhar Alam <redflame1510@msn.com>
2017-03-23 18:33:35 -0400

    Modified a module to a mixin instead of it's previous stand alone state

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -16,3 +16,33 @@ p Shout.yell_happily("Hahahaha")
 =end
 
 # mix in module
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + " *JOYOUS*" + "^^.^^"
+	end
+end
+
+class Tiger
+	include Shout
+end
+
+class Lion
+	include Shout
+end
+
+tiger = Tiger.new
+lion = Lion.new
+
+roar1 = tiger.yell_angrily("Rawwr")
+roar2 = tiger.yell_happily("Rawwr")
+roar3 = lion.yell_angrily("Rawwr")
+roar4 = lion.yell_happily("Rawwr")
+
+puts "Happy Tiger: #{roar2}, Angry Tiger: #{roar1}.."
+puts "--------------------------------------"
+puts "Happy Lion: #{roar4}, Angry Lion: #{roar3}.."
