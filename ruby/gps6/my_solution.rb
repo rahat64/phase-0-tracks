c527728dc85393c26e2b3095090450b6de406716
Iftekhar Alam <redflame1510@msn.com>
2017-03-29 02:47:14 -0400

    Refactor code to prevent doing repetitive work

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -5,7 +5,7 @@
 
 # EXPLANATION OF require_relative
 # Imports code from one file to another so the user doesn't have
-# to recall previous code.
+# to rewrite code from previous files.
 require_relative 'state_data'
 
 class VirusPredictor
@@ -20,16 +20,21 @@ class VirusPredictor
 # wrapper method that calls upon the predicted_deaths
 # and speed_of_spread methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths #(@population_density, @population, @state)
+    speed_of_spread #(@population_density, @state)
   end
 
-  private
+  private # This means all methods below private won't be acessible
+          # outside of this class so it's not a good idea to move 
+          # private above virus_effects because we need access to 
+          # that method outside our class in order to get the 
+          # final summary of both methods below.
 
 # prints out predicted number of deaths according to the calculations
 # that are set up in the if, elsif, else conditions.
-  def predicted_deaths(population_density, population, state)
+  #def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
+  def predicted_deaths
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
@@ -49,9 +54,10 @@ class VirusPredictor
 # responsible for increasing speed relative to population density of 200
 # and increases by .5 everytime population decreases by 50 then prints
 # out a string.
-  def speed_of_spread(population_density, state) #in months
+  #def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
+  def speed_of_spread
     speed = 0.0
 
     if @population_density >= 200
@@ -90,16 +96,11 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 =end
-=begin
-STATE_DATA.each do |key, value|
-  value.each do |k,v|
-    run_all = VirusPredictor.new(key, k, v)
-    p run_all
-    #run_all.virus_effects
-  end
-end
-=end
 
+# my problem solving approach to run disaster reports consecutively for
+# all 50 states in a non dry approach. Start by isolating the two 
+# values in the hash by calling upon them with their symbol call ':'
+# and assign them to two different values inside the loop.
 STATE_DATA.each do |key, value|
   pop_d = value[:population_density]
   pop = value[:population]
@@ -110,3 +111,44 @@ end
 
 #=======================================================================
 # Reflection Section
+=begin
+* What are the differences between the two different hash syntaxes 
+  shown in the state_data file?
+    - The two syntax used are the hash rocket which is '=>', and
+      the symbol which is just the colon ':' placed after the key. Both
+      essentially achieve the same purpose which is to assign a value
+      of something to the key. One thing to note though is that the 
+      key assignments via symbol cannot be assigned with strings.
+* What does require_relative do? How is it different from require?
+    - require_relative is used to retrieve codes from another file
+      so it saves the user from having to rewrite previous code from
+      that file and call upon it's contents directly. The difference
+      between require and require_relative is that require_relative 
+      complements require by allowing the user to load a file that
+      is relative to the file containing the require_relative statement.
+      When using require to load a file, this usually accesses 
+      functionality that has been properly installed, and made accesible.
+      Require does not offer a good solution for loading files within
+      the project's code. This may be useful during a development phase
+      for accessing test dat, or even accessing files that are locked
+      away inside a project, not intended for outside use.
+* What are some ways to iterate through a hash?
+    - The most efficient way is to use a .each do loop. 
+      There's also the sort.map option available.
+* When refactoring virus_effects, what stood out to you about the 
+  variables, if anything?
+    - I noticed that virus_effects doesn't take any arguments. In 
+      contrast, the private methods called inside virus_effects do. 
+      So I decided to pay attention to the arguments inside the private
+      methods and came to the realization that they are no different 
+      than the ones being called in the initialize method which makes
+      them meaningless cause I already have those values stored once
+      the instance has been initialized so I decided to get rid of the
+      arguments for both private methods and the code still ran the same.
+* What concept did you most solidify in this challenge?
+    - I solidified my debugging skills to make sure I print out every
+      output before the error to get a visual understanding of what's 
+      being stored. Furthermore, I also found a way to improvise my
+      nested method calls for isolating seperate values.
+
+=end
