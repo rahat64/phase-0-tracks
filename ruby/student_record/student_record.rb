d193ce0dd759509b9f9f35b4d247c06540a4840a
Iftekhar Alam <redflame1510@msn.com>
2017-04-10 18:50:56 -0400

    add methods to insert, update and delete students from table

--- ruby/student_record/student_record.rb
+++ ruby/student_record/student_record.rb
@@ -10,13 +10,48 @@ require 'faker'
 
 # create database
 records = SQLite3::Database.new("students.db")
+records.results_as_hash = true
 
 # string delimiters
 create_table_cmd = <<-SQL 
 	CREATE TABLE IF NOT EXISTS students(
 		id INTEGER PRIMARY KEY,
-		name VARCHAR(255)
+		identity VARCHAR(255)
 	)
 SQL
 
 records.execute(create_table_cmd)
+
+# add new students into the table as well as update their names
+# or even remove them
+
+def new_student(records, identity)
+	records.execute("INSERT INTO students (identity) VALUES (?)", 
+	[identity])
+end
+
+def change_name(records, identity)
+	records.execute("UPDATE students SET identity=? WHERE identity=?",
+	[identity])
+end
+
+def remove_student(records, identity)
+	records.execute("DELETE FROM students WHERE identity=?", 
+	[identity])
+end
+
+#just insert random data for now
+=begin
+5.times do
+	new_student(records, Faker::Name.name)
+end
+=end
+
+records.delete_if {|key, value| key >= 'Ms. Michel Hayes'}
+
+command = records.execute("SELECT * FROM students")
+puts command.class 
+p command
+
+# User interactivity
+
