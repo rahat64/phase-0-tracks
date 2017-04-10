00d4cb3aa0a615f7ac479a694cf2ceeba15cf049
Iftekhar Alam <redflame1510@msn.com>
2017-04-10 16:28:28 -0400

    Add a table to the database

--- ruby/student_record/student_record.rb
+++ ruby/student_record/student_record.rb
@@ -1,4 +1,7 @@
-
+# Purpose is to keep a record of all students for a single class
+# Unfortunately it's not entirely ambitious as to do the same
+# thing for the entire school but just the basic framework
+# for a singular classroom which can be applied to all classrooms
 
 
 # require gems
@@ -7,3 +10,13 @@ require 'faker'
 
 # create database
 records = SQLite3::Database.new("students.db")
+
+# string delimiters
+create_table_cmd = <<-SQL 
+	CREATE TABLE IF NOT EXISTS students(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255)
+	)
+SQL
+
+records.execute(create_table_cmd)
